bfca3358b8c641d484a167cb3e72fb64da7c03b7
Curtis Crawford <curtiscraw@gmail.com>
2018-01-25 19:01:27 -0800

    part c done and results in a dedicated file runtime: ~3 minutes

--- lab1/part_c.py
+++ lab1/part_c.py
@@ -1,6 +1,6 @@
 """
 Curtis Crawford, 805024638
-PARTNER?
+Abdullah-Al-Zubaer Imran, 
 
 EE219, Winter 2018
 Project 1
@@ -34,8 +34,7 @@ trainset = fetch_20newsgroups(subset='train', shuffle=True, random_state=42);
 testset = fetch_20newsgroups(subset='test', shuffle=True, random_state=42);
 
 cset = [' ']*20
-print max(fullset.target)
-print len(cset)
+
 #convert C into a list of 20 total values
 for i in range(0, len(fullset.target)):
     target = fullset.target[i]
@@ -50,12 +49,13 @@ class stemCV(text.CountVectorizer):
         analyzer = super(stemCV, self).build_analyzer()
         return lambda doc: ([stemmer.stem(t) for t in analyzer(doc)])
 
+#vectorizer = text.CountVectorizer(min_df=this_df, stop_words='english')
 vectorizer = stemCV(min_df=this_df, stop_words='english')
 
 #tokenize, vectorize the stemmed data list
 X = vectorizer.fit_transform(cset)
 
-#find TFxIDF
+#find TFxICF
 tfidf_transformer = text.TfidfTransformer()
 X_tfidf = tfidf_transformer.fit_transform(X)
 
@@ -63,10 +63,10 @@ X_tfidf = tfidf_transformer.fit_transform(X)
 for cat in ['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware', 'misc.forsale', 'soc.religion.christian']:
     idx = fullset.target_names.index(cat)
     print cat + ":"
-    row = X_tfid.toarray()[idx]
+    row = X_tfidf.toarray()[idx].tolist()
+
     for i in range(10):
-        idx_mc = max(row)
-        print str(idx_mc)
+        idx_mc = row.index(max(row))
         most_common = vectorizer.get_feature_names()[idx_mc]
-        print str(i) + "th most common item is: " + str(most_common)
-        row[most_com] = min(row)
+        print "\t" + str(i) + "th most common item is: " + str(most_common)
+        row[idx_mc] = min(row)
